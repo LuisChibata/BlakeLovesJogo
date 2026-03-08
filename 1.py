f4d65901a375167f9a2b5272a64244e36455187d
Blake <ieatchez1234@gmail.com>
2026-03-08 15:55:05 +1100

    Yes

--- 1.py
+++ 1.py
@@ -1,2 +1,11 @@
-username = input("Please enter your username: ")
-print(username)
+import my_api
+
+def identity(name, age):
+    print(f"your name is {name} and your age is {age}")
+identity("Blake", 17)
+
+def sum(num1, num2):
+    print(num1+num2)
+sum(1, 5)
+
+print(my_api.add(1, 9))
